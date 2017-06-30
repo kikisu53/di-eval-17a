040bd27b8d318e40bb0ee44ff80778e8d89eb875
kiki <hcqp53@hotmail.com>
2017-06-30 14:17:45 +0800

    Finish kv.js

--- api/controllers/kv.js
+++ api/controllers/kv.js
@@ -1,19 +1,43 @@
-let now = '2017-06-29T07:36:17.653Z'
+var URLSafeBase64 = require('urlsafe-base64');
+
+let now = new Date().toISOString();
+//let now = '2017-06-29T07:36:17.653Z';
+
+var key = req => {
+  var key = req.path.replace('/kv/','');
+  return URLSafeBase64.validate(key) ? key : false;
+}
+
+var db = {};
 
 function getKEY(req, res) {
-  res.json(404, {
-    VALUE: "hello world",
-    TS: now,
-  })
+  var usekey = key(req);
+  if(!usekey) res.json(400, { "message": "給人看的錯誤說明" });
+  db[usekey]
+  ? res.json(200, { 
+      VALUE: db[usekey],
+      TS: now,
+    })
+  : res.json(404, { "message": "給人看的錯誤說明" })
 }
 
 function deleteKEY(req, res) {
-  res.json(200, {
-    TS: now,
-  })
+  var usekey = key(req);
+  if(!usekey) res.json(400, { "message": "給人看的錯誤說明" });
+  var json = {};
+  if(db[usekey]) {
+    json['OLD_VALUE'] = db[usekey];
+    delete db[usekey];
+  }
+  json['TS'] = now;
+  res.json(200, json)
 }
 
 function postKEY(req, res) {
+  var usekey = key(req), useval = req.body.VALUE;
+  if(useval.match(/[^1-9a-zA-Z=+/]/)!==null
+    || !usekey) res.json(400, { "message": "給人看的錯誤說明" });
+  db[usekey] = req.body.VALUE;
   res.json(200, {
     TS: now,
   })
